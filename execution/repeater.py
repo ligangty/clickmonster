99bb08567912ddf9fa517ce5e7c793720348e5dd
Gang Li <gli@redhat.com>
2017-05-09 23:13:57 +0800

    format

--- execution/repeater.py
+++ execution/repeater.py
@@ -1,21 +1,21 @@
 from threading import Timer
 import atexit
 
+
 class Repeat(object):
+  count = 0
 
-    count = 0
-    @staticmethod
-    def repeat(rep, delay, func):
-        "repeat func rep times with a delay given in seconds"
+  @staticmethod
+  def repeat(rep, delay, func):
+    "repeat func rep times with a delay given in seconds"
 
-        if Repeat.count < rep:
-            # call func, you might want to add args here
-            func()
-            Repeat.count += 1
-            # setup a timer which calls repeat recursively
-            # again, if you need args for func, you have to add them here
-            timer = Timer(delay, Repeat.repeat, (rep, delay, func))
-            # register timer.cancel to stop the timer when you exit the interpreter
-            atexit.register(timer.cancel)
-            timer.start()
-
+    if Repeat.count < rep:
+      # call func, you might want to add args here
+      func()
+      Repeat.count += 1
+      # setup a timer which calls repeat recursively
+      # again, if you need args for func, you have to add them here
+      timer = Timer(delay, Repeat.repeat, (rep, delay, func))
+      # register timer.cancel to stop the timer when you exit the interpreter
+      atexit.register(timer.cancel)
+      timer.start()
